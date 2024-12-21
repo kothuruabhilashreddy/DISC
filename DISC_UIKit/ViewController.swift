c571b1137c64f6b602505e8fbeed441ed951b69f
Kothuru Abhilash Reddy <kothuruabhilashreddy@gmail.com>
2024-12-21 11:05:40 -0500

    Initial Commit

---
+++ DISC_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DISC_UIKit
+//
+//  Created by Mohammed danish on 12/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
